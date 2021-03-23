ba8053e8082b71c96c873311dc82f6ebeccd8b59
Henrik Jangefelt <henke9105@hotmail.com>
2021-03-23 21:36:04 +0100

    importing routes for products and users

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,10 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 
+// Import Routes
+const productRoutes = require('./routes/products')
+const userRoutes = require('./routes/users')
+
 
 mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
@@ -13,8 +17,10 @@ const db = mongoose.connection
 db.on('error', (error) => console.log(error)) // problem connecting...
 db.once('open', () => console.log('Successfully connected to DB'))
 
-
 app.use(express.json()) 
+
+app.use('/api/v1/products', productRoutes)
+app.use('/api/v1/users', userRoutes)
 
 
 const PORT = process.env.PORT || 5000
